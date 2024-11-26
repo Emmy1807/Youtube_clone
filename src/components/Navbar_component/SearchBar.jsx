496a797d6b43bc043f95b2593734225f11031216
Emmy1807 <emmanuelonuh141@gmail.com>
2024-11-26 16:11:33 +0100

    Search Vidoes

--- src/components/Navbar_component/SearchBar.jsx
+++ src/components/Navbar_component/SearchBar.jsx
@@ -3,30 +3,56 @@
 import React from 'react'
 import { SearchVideos } from '@/util/fetchData'
 import { useState } from 'react'
+import Image from 'next/image'
 
 const SearchBar = () => {
 
-    const [ count, setCount ] = useState(0);
+  const [loading, setLoading] = useState(false)
+  const [result, setResult] = useState([])
+  const [query, setQuery] = useState("") 
 
-    function update() {
-        setCount(count+1)
-    }
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+
+    const data = await SearchVideos (`search/?query=${query}`)
+    setResult(data.videos)
+   console.log(data)
+   setLoading(false) 
+  }
+
+    
   return (
     <div>
-    <form> 
-      <input type="text" placeholder='Search...'  />
+    <form onSubmit={handleSearch}> 
+      <input type="text" placeholder='Search...' 
+       value={query}
+       onChange={(e) =>setQuery(e.target.value)}
+       />
     <button>
         Search
     </button>
     </form>  
 
-       <h1>{count}</h1> 
-       <button onClick={update}>
-        update count 
-       </button> 
+  {
+    loading ? <p>Loading</p> : null
+  }  
+
+  {
+    result.map((eachItem, index, array) => (
+      <div key={index}>
+        <h1>{eachItem.title}</h1>
+        <h2>{eachItem.video_length}</h2>
+        <h3>{eachItem.number_of_views}</h3>
+        <img src={eachItem.thumbnails[0].url} alt="image" />
+      </div>
+    ))
+  }    
     </div>
       
   )
 }
 
 export default SearchBar
+
+
--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -1,15 +1,15 @@
 const BASE_URL =  'https://youtube-v2.p.rapidapi.com'
 
-const SearchVideos = () => {
-    const res = fetch(`${BASE_URL}/${query}`, {
-        method: "Get",
+export const SearchVideos = async (param) => {
+    const res = await fetch(`${BASE_URL}/${param}`, {
+        method: 'GET',
         headers: {
             'x-rapidapi-key': '6d5d13f287mshd7229a7987f29f2p1947edjsn3acc82e27c4c',
 		'x-rapidapi-host': 'youtube-v2.p.rapidapi.com'
         }
     })
 
-    const result = res.json();
+    const result = await res.json();
     return result;
      
 }

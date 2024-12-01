9ca6179694c4a79bbfc0dd80b96fe89f5b5fa3d6
Emmy1807 <emmanuelonuh141@gmail.com>
2024-12-01 02:55:14 +0100

    images

--- src/components/SideContent/SideBar.js
+++ src/components/SideContent/SideBar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import styles from './SideBar.module.css'
 
 const SideBar = () => {
   return (
     <div>
-      SideBar
+      <Image src='/download (8).png' width={20} height={20}
+      alt='image'/>
+      <p>Home</p>
     </div>
   )
 }
